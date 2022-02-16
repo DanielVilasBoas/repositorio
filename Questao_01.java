b684f3d53350487c4ae4c11dd8bbef7ab6c2ecd8
Daniel Vilas Boas <dvbs15@hotmail.com>
2022-02-16 16:54:38 -0300

    Update Questao_01.java

--- Questao_01.java
+++ Questao_01.java
@@ -1,15 +1,16 @@
+import javax.swing.JOptionPane;
 
 public class Questao_01 {
 
 	public static void main(String[] args) {
-		int n = 6;
+		String n = JOptionPane.showInputDialog("Informe o número de degraus para formar a escada");
 		
-		for (int i = n; i > 0 ; i--) {
+		for (int i = Integer.parseInt(n); i > 0 ; i--) {
 			String star = "";
 			for (int x = i; x > 1; x--) {
 				star = star.concat(" ");
 			}
-			for (int j = 0; j <=-(i - n); j++) {
+			for (int j = 0; j <=-(i - Integer.parseInt(n)); j++) {
 				star = star.concat("*");
 				}
 			System.out.println(star);
