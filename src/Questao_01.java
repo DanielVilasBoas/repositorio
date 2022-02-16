21b25c359dfb7ad9b952c8719bd81aa644c47f70
Daniel Vilas Boas <dvbs15@hotmail.com>
2022-02-16 16:11:33 -0300

    teste

---
+++ src/Questao_01.java
@@ -0,0 +1,19 @@
+
+
+public class Questao_01 {
+
+	public static void main(String[] args) {
+		int n = 6;
+		
+		for (int i = n; i > 0 ; i--) {
+			String star = "";
+			for (int x = i; x > 1; x--) {
+				star = star.concat(" ");
+			}
+			for (int j = 0; j <=-(i - n); j++) {
+				star = star.concat("*");
+				}
+			System.out.println(star);
+		}	
+	}
+}
