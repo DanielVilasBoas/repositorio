e381d2487e46085c66487218008b13d30460ee5a
Daniel Vilas Boas <dvbs15@hotmail.com>
2022-02-16 13:44:22 -0300

    Questão 02
    Débora digitou uma string aleatória no campo de senha, porém ela não tem certeza se é uma senha forte. Para ajudar Débora, construa um algoritmo que informe qual é o número mínimo de caracteres que devem ser adicionados para uma string qualquer ser considerada segura.

---
+++ questao_02.java
@@ -0,0 +1,83 @@
+package questao_02;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.swing.JOptionPane;
+
+public class questao_02 {
+
+	public static void main (String args[]) {
+		int hits = 6;
+		
+		String password = JOptionPane.showInputDialog("Informe uma senha");
+		
+		boolean isDigit = false;
+		boolean isCapital = false;
+		boolean isTiny = false;
+		boolean isSymbol = false;
+		boolean isAny_1 = false;
+		boolean isAny_2 = false;
+		
+		for(int i =0; i<password.toCharArray().length; i++) {
+			String letter =  String.valueOf(password.toCharArray()[i]);
+			
+			if (!isDigit && Pattern.compile("[0-9]")
+					.matcher(letter)
+					.matches()
+					) {
+				hits--;
+				isDigit = true;
+				continue;
+			}
+			
+			
+			if (!isCapital && Pattern.compile("[A-Z]")
+					.matcher(letter)
+					.matches()
+					) {
+				hits--;
+				isCapital = true;
+				continue;
+			}
+		
+			if (!isTiny && Pattern.compile("[a-z]")
+					.matcher(letter)
+					.matches()
+					) {
+				hits--;
+				isTiny = true;
+				continue;
+			}
+		 
+			if (!isSymbol && Pattern.compile("[!@#$%^&*()-+]")
+					.matcher(letter)
+					.matches()
+			) {
+				hits--;
+				isSymbol = true;
+				continue;
+			}
+			
+			if (!isAny_1 && Pattern.compile("[a-zA-Z0-9!@#$%^&*()-+]")
+					.matcher(letter)
+					.matches()
+					) {
+				hits--;
+				isAny_1 = true;
+				continue;
+			}
+			
+			if (!isAny_2 && Pattern.compile("[a-zA-Z0-9!@#$%^&*()-+]")
+					.matcher(letter)
+					.matches()
+					) {
+				hits--;
+				isAny_2 = true;
+				continue;
+			}
+		}	
+		
+		System.out.println(hits);
+	}
+}
